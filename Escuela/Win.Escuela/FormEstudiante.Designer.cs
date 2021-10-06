485847298b6017e2c9466ef6aa26d28f6af5b667
delmi <delmi@LAPTOP-04KM7RKL>
2021-10-05 19:52:29 -0600

    Proyecto Escuela

--- Escuela/Win.Escuela/FormEstudiante.Designer.cs
+++ Escuela/Win.Escuela/FormEstudiante.Designer.cs
@@ -362,7 +362,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(658, 456);
             this.Controls.Add(activoLabel);
             this.Controls.Add(this.activoCheckBox);
