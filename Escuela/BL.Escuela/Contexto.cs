0177cbcc9d1dcdce5f95eec57a752d99c7983c23
delmi <delmi@LAPTOP-04KM7RKL>
2021-10-23 14:54:23 -0600

    Agregando Proyecto

---
+++ Escuela/BL.Escuela/Contexto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Escuela
+{
+    public class Contexto: DbContext //Herencia para utilizar bd
+    {
+        public Contexto(): base("RegistroEstudiantes")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+        public DbSet<Estudiante> Estudiantes { get; set; } // Lista
+    }
+}
